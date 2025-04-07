b4e70438e3f4e16a319511bde336c299e1220bf5
mimi202501 <cortados13@gmail.com>
2025-04-07 12:31:14 +0200

    codigo ascii

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ if __name__ == "__main__":
     print(chr(98))
     print("=======")
     print(ord('c'))
+    print("codigos ascii")
+    for i in range(256):
+        if 32 <= i <= 255:
+            print(f"{i}: {chr(i)}")
+        else:
+            print(f"caracter no imprmible")
